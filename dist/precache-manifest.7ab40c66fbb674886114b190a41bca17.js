cc31d12f45c051aa2f8de9fd265e11c19f7bbbb8
sevta <sevtasaputra@gmail.com>
2020-04-02 21:42:38 +0700

    add dist folder

---
+++ dist/precache-manifest.7ab40c66fbb674886114b190a41bca17.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba962bb7b9c19953393",
+    "url": "/css/app.8c0ad3a3.css"
+  },
+  {
+    "revision": "176e52156f90fa90a49239034036fe1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b23c955922584e6cb7a4",
+    "url": "/js/about.0c8dd410.js"
+  },
+  {
+    "revision": "2ba962bb7b9c19953393",
+    "url": "/js/app.41568314.js"
+  },
+  {
+    "revision": "0345bb34f82595f9aff2",
+    "url": "/js/chunk-vendors.58601c06.js"
+  },
+  {
+    "revision": "3a2bf6ce906c796cb8db4a80054ed214",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
